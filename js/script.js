3ed53fd7ced863460a9b0989ff9206fe51ea6418
Heber Cordova <u201924464@upc.edu.pe>
2022-04-25 03:11:05 -0500

    fix: Fixed html injection problems
    Fixed problems in html and javascript

--- js/script.js
+++ js/script.js
@@ -7,4 +7,21 @@ import { setFooterNavContent } from "./footerNav.js";
     setHeaderNavEvent();
     SetBurgerNavIconEvent();
     setFooterNavContent();
+
+    const paths = [
+        ["home.html", "Home"],
+        ["aboutus.html", "About-us"],
+        ["plans.html", "Plans"],
+        ["services.html", "Services"],
+        ["help.html", "Help"]];
+
+    paths.forEach(path => {
+        fetch(`../${path[0]}`)
+            .then(response => {
+                return response.text()
+            })
+            .then(data => {
+                document.querySelector("#"+path[1]).innerHTML = data;
+            });
+    })
 })();
